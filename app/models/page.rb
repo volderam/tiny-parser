282582467f13a189b8b7824e539bee620095715f
volderam <volderam@yandex.com>
2017-06-23 19:26:57 +0300

    Add parsing

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,14 @@
 class Page < ApplicationRecord
 	validates :url, presence: true
+	after_create :parse_content
+
+	def parse_content
+		content = {}
+		doc = Nokogiri::HTML(open(url))
+		%w{ h1 h2 h3 href}.each do |tag|
+			content[tag] = tag == "href" ? doc.xpath("//a/@#{tag}").map(&:text) : doc.xpath("//#{tag}").map(&:text)
+		end
+		self.content = content
+		self.save
+	end
 end
